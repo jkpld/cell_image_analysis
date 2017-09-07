61a60301207915b0b6d8162e4f2d7db459d9fef2
James Kapaldo <james.kapaldo@gmail.com>
2017-09-07 00:52:30 -0400

    Feature: add ability to choose GPU or CPU
    Also
    - clean up code
    - add documentation

--- measureGranularity.m
+++ measureGranularity.m
@@ -1,122 +1,137 @@
 function G = measureGranularity(I,mask,options)
+% MEASUREGRANULARITY Compute the granular spectrum for each object in mask
+% using the image I.
+%
+% G = measureGranularity(I,mask,options)
+%
+% Input
+%   I : image
+%   maks : object mask
+%   options : structure with the following fields
+%     SubSampleSize - the factor to re-size the image by before computing
+%       the spectrum. 
+%     BackgroundSampleSize - the factor to re-size the image before
+%       computing the image background
+%     ElementSize - The size of the primary elements
+%     GranularSpectrumLength - The length of the granular spectrum
+%     Use_GPU : (boolean) Determine if to use gpu to speed up. (speed up
+%       factor is ~3 on my computer)
+%
+% Output
+%   G : granular spectrum. N x GranularSpectrumLength, where N is the
+%     number of objects. The object number corresponds to the object number
+%     given by bwlabel/bwconncomp/...
+
+% James Kapaldo
 
 SubSampleSize = options.SubSampleSize;
 BackgroundSampleSize = options.BackgroundSampleSize;
 ElementSize = options.ElementSize;
 GranularSpectrumLength = options.GranularSpectrumLength;
+Use_GPU = options.Use_GPU;
 
-I = double(I);
+sz = size(I);
+
 mask = mask>0;
+I = single(I);
+% The im2uint8 function used below (which calls a mex function) requires
+% the image to be in the range [0,1]
+I = I - min(I(:));
+I = I/max(I(:));
+
 
 %% Subsample
+
+if Use_GPU
+    I = gpuArray(I);
+    mask = gpuArray(mask);
+end
+
 if SubSampleSize ~= 1
-    I = imresize(I,SubSampleSize,'bilinear');
-    mask = imresize(mask,SubSampleSize,'bilinear')>0.9;
+    I = imresize(I, SubSampleSize);
+    mask = imresize(mask, SubSampleSize)>0.5;
 end
 
 %% Background correct
 
-% Create background structuring element.
-%   -> instead of resizing the image, use a larger structering element
-% se = strel('disk',round(ElementSize*(1/BackgroundSampleSize)));
-
-% I_BG = imopen_mask(I,se,mask);
-% I_FG = I - I_BG;
-% I(mask) = I_FG(mask);
-
-
 % Resize image and mask
 if BackgroundSampleSize ~= 1
-    Ism = imresize(I,BackgroundSampleSize,'bilinear');
-    mask_sm = imresize(mask,BackgroundSampleSize,'bilinear')>0.5;
+    Ism = imresize(I, BackgroundSampleSize);
+    mask_sm = imresize(mask, BackgroundSampleSize)>0.5;
 else
     Ism = I;
     mask_sm = mask;
 end
-mask_sm_d = gpuArray(mask_sm);
 
-% Remove background
+% Compute background
+% - gpu erosion/dilation require uint8 and resizing requires single/double
+%   so need to cast to different types
+% - if using a gpu, then it could be necessary to conserve memory, so
+% remove temporary variables when they are no longer needed.
 se = strel('disk',ElementSize);
-minIsm = min(Ism(:));
-maxIsm = max(Ism(:));
-Ism = uint8(255*(Ism-minIsm)/(maxIsm-minIsm));
-Ism_d = gpuArray(Ism);
-Ism_BG_d = imopen_mask(Ism_d,se,~mask_sm_d);
-I_FG = Ism - gather(Ism_BG_d); %*(maxIsm-minIsm)/255 + minIsm;
-clear Ism_d Ism_BG_d mask_sm_d
+Ism8 = im2uint8(Ism);                   clear Ism
+BGsm8 = imopen_mask(Ism8,se,~mask_sm);  clear Ism8 mask_sm
+BGsm = single(BGsm8);                   clear BGsm8
+BG = imresize(BGsm,sz);                 clear BGsm
 
-% Un-resize
-I = imresize(I_FG,size(I),'bilinear');
+% Set image to uint8 to make the same scale as BG and subtract BG
+I = im2uint8(I) - uint8(BG);            clear BG
 
-%% Convert image to uint8
-
-% I(~mask) = 0;% = I.*mask;
-% I = I - min(I(mask));
-% I = uint8(255*I/max(I(mask)));
-
-
-%% Label objects
-
-% Label objects
-% CC = bwconncomp(mask);
-% L = labelmatrix(CC);
+%% Label objects 
 L = bwlabel(mask);
 
 L = L(:);
 FG = L>0;
 L = L(FG);
 
-
 %% Compute granular spectrum
 
+% just need the ~mask from now on.
+notmask = ~mask;                        clear mask;
 
-
-% Send image to GPU
-I_d = gpuArray(I);
-ero_d = gpuArray(I);
-notmask_d = gpuArray(~mask);
-% L_d = gpuArray(L);
-% FG_d = gpuArray(FG);
-% N_d = gpuArray(CC.NumObjects);
-
-% I_d = I;
-% ero_d = I;
-% ero_d(~mask) = 0;
-% mask_d = mask;
-
-N = max(L);
+ero = I; % copy the image to hold the erosions
+N = max(L); % get the number of objects
 
 % Create structure element
-se_d = gpuArray([0 1 0; 1 1 1; 0 1 0]);%strel('diamond',1);
-% se = strel('diamond',1);
+se = gpuArray([0 1 0; 1 1 1; 0 1 0]);
 
 % Starting object mean intensity
-startmean = accumarray(L,I(FG),[N,1],@sum);
+Is = single(I); % need single array if on gpu
+startmean = accumarray(L,Is(FG),[N,1],@sum);    clear Is
 currentmean = startmean;
 
 % initialize granular spectrum
-G = zeros(N,GranularSpectrumLength);
+if Use_GPU
+    G = gpuArray.zeros(N,GranularSpectrumLength);
+else
+    G = zeros(N,GranularSpectrumLength);
+end
 
 % compute granular spectrum on gpu
 for i = 1:GranularSpectrumLength
     prevmean = currentmean;
-    ero_d = imerode_mask(ero_d,se_d,notmask_d);
-    rec_d = imreconstruct(ero_d,I_d,4);
-    rec = gather(rec_d);
-%     rec = rec_d;
+    ero = imerode_mask(ero,se,notmask);
+    rec = imreconstruct(ero,I,4);
+    
+    if Use_GPU
+        % If using a GPU then we need single arrays for accumarray.
+        recS = single(rec);
+        currentmean = accumarray(L,recS(FG),[N,1],@sum);
+        clear recS
+    else
+        currentmean = accumarray(L,rec(FG),[N,1],@sum);
+    end
 
-    currentmean = accumarray(L,rec(FG),[N,1],@sum);
-%     currentmean_d = accumarray(L_d,single(rec_d(FG_d)),[N_d,1],@sum);
-%     G(:,i) = prevmean - gather(currentmean_d);
     G(:,i) = prevmean - currentmean;
+    clear rec
 end
-
-% clean up memory
-clear ero_d I_d rec_d se_d notmask_d
 
 % normalize granular spectrum
 G = 100*(G ./ startmean);
 
+if Use_GPU
+    G = gather(G);
+end
 
 end
 
