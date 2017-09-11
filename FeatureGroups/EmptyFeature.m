28760e57429da354d0f85a36201db5a74704112b
James Kapaldo <james.kapaldo@gmail.com>
2017-09-11 14:21:08 -0400

    Add EmptyFeature class

---
+++ FeatureGroups/EmptyFeature.m
@@ -0,0 +1,46 @@
+classdef EmptyFeature < FeatureGroup
+    % EMPTYFEATURE Empty feature group to act as a place holder in
+    % heterogenous arrays of FeatureGroup's
+    %
+    % Properties :
+    %
+    % Channel - Not used.
+    % requiredOptions - Empty structure
+    %
+    % Methods :
+    %
+    % Compute(~, ~) - Return an empty matrix
+    %   x = EmptyFeature.Compute([], [])
+    
+    properties (Dependent, SetAccess = protected)
+        FeatureNames
+    end
+    methods
+        function obj = EmptyFeature(channel,options)
+            obj.GroupName = class(obj);
+
+            % Default options
+            defaultOptions = struct();
+            obj.requiredOptions = fieldnames(defaultOptions)';
+            
+            if nargin < 2
+                options = defaultOptions;
+            end
+            obj.Options = options;
+            
+            if nargin >= 1
+                if ~strcmp(channel, '') || ~isempty(channel)
+                    warning('Location:channelNotUsed', 'The Location FeatureGroup does not operate on an image channel. Channel given will be ignored.')
+                end
+            end
+        end
+        
+        function names = get.FeatureNames(~)           
+            names = "";
+        end
+        
+        function x = Compute(~, ~, ~)
+            x = [];
+        end
+    end
+end
