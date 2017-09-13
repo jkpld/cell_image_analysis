f311799bc842903b4105dedb285959dcb2f17de1
James Kapaldo <james.kapaldo@gmail.com>
2017-09-12 23:16:07 -0400

    Adjust template

--- ObjectPartitioner.m
+++ ObjectPartitioner.m
@@ -1,14 +1,12 @@
 classdef (Abstract) ObjectPartitioner
     properties
-        Options
-        Channel = ''
-    end
-    
-    methods (Abstract, Static)
-        trueFalse = attemptPartioning(obj)
+        Channel(1,1) string = ""
     end
     
     methods (Abstract)
-        mask_partioned = partition(obj)
+        mask_partioned = partition(obj, BW, I, Image_Offset)
+        
+        tf = keepObject(obj) % Take in information about each object (before partitioning) and determine if the object should be kept (true) or thrown away (false)
+        tf = attemptPartitioning(obj) % Take in information about each object (before partitioning) and determine if partitioning should be attempted (E.g. if the object is smaller than some value, then you might know you do not need to partition in.)
     end
 end
