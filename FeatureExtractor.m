ced325a8d22e763ed35b06da1a9d784ceb67e93d
James Kapaldo <james.kapaldo@gmail.com>
2017-09-27 00:32:39 -0400

    Fix: add in image offset

--- FeatureExtractor.m
+++ FeatureExtractor.m
@@ -16,9 +16,13 @@ classdef FeatureExtractor
             obj.featureGroups(end+1) = group;
         end
         
-        function [x, names, mask] = Compute(obj, mask, I)
+        function [x, names, mask] = Compute(obj, mask, I, Image_Offset)
             if size(I,3) ~= numel(obj.channels)
                 error('FeatureExtractor:channelsDimensionMismatch','The input image I must have the same number of channels (and be in the same order) as the channels cell array used when creating the object.');
+            end
+            
+            if nargin < 4
+                Image_Offset = zeros(1,2,'single');
             end
             
             % Partition the objects?
@@ -44,16 +48,27 @@ classdef FeatureExtractor
             validChannels = ["", obj.channels];
             groups = obj.featureGroups(~isempty(obj.featureGroups) & ismember([obj.featureGroups.Channel],validChannels));
             
+            if numel(groups) == 0
+                x = [];
+                names = [];
+                warning('Compute:noValidGroups','There are no valid groups - either the FeatureGroups are EmptyFeatures or the FeatureGroup channel names to not match the FeatureExtractor channel names.')
+                return;
+            end
+            
             % Get feature names
             names = {groups.FeatureNames};
             num_xi = cellfun(@numel, names);
+            names = cat(2,names{:});
             
             % Initialize feature array
             x = zeros(max(L(:)), sum(num_xi), 'single');
 
+            if size(x,1) == 0
+                return;
+            end
+            
             % feature end indices
             num_xi = cumsum([0,num_xi]);
-
             
             for i = 1:numel(groups)
                 ch = obj.channels == groups(i).Channel;
@@ -63,10 +78,11 @@ classdef FeatureExtractor
                     xi = groups(i).Compute(I(:,:,ch), L);
                 end
 
+                if isa(groups(i),'Location')
+                    xi(:,1:4) = xi(:,1:4) + [Image_Offset, Image_Offset];
+                end
                 x(:, num_xi(i)+1:num_xi(i+1)) = xi;
             end
-            
-            names = cat(2,names{:});
         end
     end 
 end
