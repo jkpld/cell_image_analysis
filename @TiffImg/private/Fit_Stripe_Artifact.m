49e92c1fb26e66f218f7b38202aa62090132fcba
James Kapaldo <james.kapaldo@gmail.com>
2017-10-08 23:29:24 -0400

    Feat: Detect if there is a stripe and update default bin size

--- @TiffImg/private/Fit_Stripe_Artifact.m
+++ @TiffImg/private/Fit_Stripe_Artifact.m
@@ -19,8 +19,13 @@ function [stripe, xCents, z, fsample] = Fit_Stripe_Artifact(x,z,varargin)
 % In general, each color channel of the image needs to be corrected.
 %
 % Optional parameters:
+%
+% approximatePeriod - The approximate period of the signal in [mm]. This
+% can be taken from the image's Stripe_Width property.
+%
 % initialBinSize - Size of the initial bin used to discretize x data. Unit
-% should be mm. Default is 0.005 mm. 
+% should be mm. The default is to set the bin size such that there are
+% approximately 100 points per bin.
 %
 % z_bounds - Bounds of the z data to use when fitting the optical
 % distortion. Default is [0.65, 1.5]. 
@@ -46,6 +51,8 @@ function [stripe, xCents, z, fsample] = Fit_Stripe_Artifact(x,z,varargin)
 % JamesKapaldo
 % August 10, 2016
 
+DEBUG = 0;
+
 % Make sure inputs are columns.
 if isrow(x)
     x = x(:);
@@ -54,13 +61,16 @@ if isrow(z)
     z = z(:);
 end
 
+tmp = rand(1);
+
 % Get input values
 p = inputParser;
 p.FunctionName = 'bg_fitOpticalDistortion';
-addParameter(p,'initialBinSize',0.005,@(t) numel(t) == 1 && t > 0 && t < max(x)-min(x));
-addParameter(p,'z_bounds',[0.65, 1.5],@(t) numel(t) == 2 && all(t > 0 & t < max(z)-min(z)) && t(1) < t(2));
+addParameter(p,'initialBinSize',tmp,@(t) numel(t) == 1 && t > 0 && t < max(x)-min(x));
+addParameter(p,'z_bounds',[0.65, 1.5],@(t) numel(t) == 2  && t(1) < t(2)); %&& all(t > 0 & t < max(z)-min(z))
 addParameter(p,'threshold',1e-4,@(t) numel(t) == 1 && t > 0);
 addParameter(p,'generatePlots',false,@(t) islogical(t) || (t==0 || t == 1) );
+addParameter(p,'approximatePeriod',nan,@(t) numel(t) == 0 && ~isinf(t));
 
 parse(p,varargin{:})
 
@@ -68,8 +78,21 @@ voxelSizes = p.Results.initialBinSize; % Sample period
 zB = p.Results.z_bounds;
 threshold = p.Results.threshold;
 generatePlots = p.Results.generatePlots;
+T = p.Results.approximatePeriod;
 
 boundEdges = prctile(x,[0,100]);
+
+
+if isequal(voxelSizes,tmp)
+    ppb = 100; % points per bin
+    rho = numel(x)/diff(boundEdges); % point density
+    voxelSizes = ppb/rho; % default bin size
+%     voxelSizes = 0.04739;
+end
+
+voxelSizes = 0.005;
+
+voxelSizes*numel(x)/diff(boundEdges)
 
 xEdges = (boundEdges(1)-voxelSizes(1)) : voxelSizes(1)  : (boundEdges(2) + voxelSizes(1));
 
@@ -126,6 +149,7 @@ end
 
 stripe(isnan(stripe) | isinf(stripe) | stripe == 0) = 1;
 
+% Power spectrum
 Y = fft(stripe);
 P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
@@ -135,13 +159,59 @@ P1(2:end-1) = 2*P1(2:end-1);
 %     line(freq, P1)
 %     ylim([0,0.1])
 
+% Autocorrelation of power spectrum
+[c,l] = xcorr(P1,'unbiased');
+
+% Select first half of the positive delay
+c = c(l>0);
+c = c(1:floor(end/2));
+
+% Remove background
+sig = c-medfilt1(c,31,'truncate');
+
+% Normalize
+sig = sig/mad(sig,1);
+
+% Compute new samply frequency
+fsample = 4*freq(find(sig>10 & sig == imdilate(sig,ones(31,1)),1,'last')+1);
+
+if generatePlots
+    figure
+    subplot(2,1,1)
+    hold on
+    plot(freq(2:numel(sig)+1),sig,'color','y')
+    xlabel('wavelength / mm^{-1}')
+    ylabel('normalized strength')
+    title('Normalized autocorrelation of power spectrum')
+    grid on
+    
+    subplot(2,1,2)
+    hold on
+    plot(freq,P1,'color','y')
+    xlabel('wavelength / mm^{-1}')
+    ylabel('power')
+    title('power spectrum')
+    grid on
+    setTheme(gcf,'dark')
+end
+
+% If the sampling frequency is empyt (no signal over 10*MAD), then no
+% stripe was detected. 
+if isempty(fsample)
+    if generatePlots
+        warning('Fit_Stripe_Artifact:noStripeDetected','No stripe was detected.')
+    end
+    stripe = ones(size(stripe));
+    z = origZ;
+    return
+end
 
 % Now repeate with proper sample frequency -------------------------------
 
 z = origZ;
 
 % fsample = 2*(max(freq(P1>0.01)) + 40); % old sample frequency - this is not a harmonic of the stripe frequency
-fsample = 3*max(freq(P1>median(P1(2:end))+2*std(P1(2:end)))); % new sample frequency - this should be a harmonic of the stripe frequency
+% fsample = 3*max(freq(P1>median(P1(2:end))+2*std(P1(2:end)))); % new sample frequency - this should be a harmonic of the stripe frequency
 
 voxelSizes = 1/fsample;
 xEdges = (boundEdges(1)-voxelSizes(1)) : voxelSizes(1)  : (boundEdges(2) + voxelSizes(1));
@@ -193,16 +263,21 @@ if generatePlots
     subplot(2,1,1)
     line(x,origZ,'Marker','.','MarkerSize',1,'LineStyle','none','Color','y')
     line(xCents,stripe,'Color','b','LineWidth',1)
-    drawnow;
     axis tight
     ylim([0.5,2.5]);
+    ylabel('data value')
+    title('Original data with computed stripe')
     
     subplot(2,1,2)
     line(x,z,'Marker','.','MarkerSize',1,'LineStyle','none','Color','y')
-    drawnow;
+%     drawnow;
     setTheme(gcf,'dark')
     axis tight
     ylim([0.5,2.5]);
+    xlabel('x / mm')
+    ylabel('data value')
+    title('Data with computed stripe removed')
+    
 end
 
 end
